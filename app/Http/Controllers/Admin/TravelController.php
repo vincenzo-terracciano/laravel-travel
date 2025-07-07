45507e4c2f2654272f3463e32e739f57c601cd49
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-07-07 19:27:16 +0200

    Add travel creation form and implement store functionality in TravelController

--- app/Http/Controllers/Admin/TravelController.php
+++ app/Http/Controllers/Admin/TravelController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Tag;
 use App\Models\Travel;
 use Illuminate\Http\Request;
 
@@ -23,7 +25,13 @@ class TravelController extends Controller
      */
     public function create()
     {
-        //
+        // prendo tutte le categorie
+        $categories = Category::all();
+
+        // prendo tutti i tag
+        $tags = Tag::all();
+
+        return view('admin.travels.create', compact('categories', 'tags'));
     }
 
     /**
@@ -31,7 +39,31 @@ class TravelController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+
+        $newTravel = new Travel();
+
+        $newTravel->title = $data["title"];
+        $newTravel->description = $data["description"];
+        $newTravel->destination_country = $data["destination_country"];
+        $newTravel->destination_city = $data["destination_city"];
+        $newTravel->departure_date = $data["departure_date"];
+        $newTravel->return_date = $data["return_date"];
+        $newTravel->category_id = $data["category_id"];
+        $newTravel->cover_image = $data["cover_image"];
+        $newTravel->is_published = isset($data["is_published"]);
+
+        $newTravel->save();
+
+        // verifico se ricevo i tag
+        if ($request->has('tags')) {
+
+            // aggiungo alla tabella pivot tutti i tag selezionati collegati al viaggio in questione
+            $newTravel->tags()->attach($data['tags']);
+        }
+
+        return redirect()->route('admin.travels.index')
+            ->with('success', 'Viaggio creato con successo');
     }
 
     /**
