a29d260c04f88af531654df8255afc4754de3b59
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-07-11 13:34:01 +0200

    Add custom navbar styles and update header layout; enhance button visibility and improve user navigation experience.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,15 +1,17 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-md">
+<nav class="navbar navbar-expand-md shadow-md custom-navbar">
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{ route('admin.home') }}">Dashboard</a>
+        <a class="navbar-brand fw-bold text-white" href="{{ route('admin.home') }}">
+            <i class="fas fa-compass me-2"></i> Dashboard
+        </a>
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarAdmin" aria-controls="navbarAdmin" aria-expanded="false"
                 aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+                <i class="hamburger fas fa-bars fs-3"></i>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarAdmin">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0 gap-2">
                 <li class="nav-item">
                     <a href="{{ route('admin.travels.index') }}" class="nav-link">Viaggi</a>
                 </li>
@@ -20,9 +22,12 @@
                     <a href="{{ route('admin.tags.index') }}" class="nav-link">Tags</a>
                 </li>
             </ul>
+
             <form method="POST" action="{{ route('logout') }}">
                 @csrf
-                <button class="btn btn-outline-light">Logout</button>
+                <button class="btn btn-outline-light d-flex align-items-center gap-1">
+                    <i class="fas fa-sign-out-alt"></i> <span class="d-none d-md-inline">Logout</span>
+                </button>
             </form>
         </div>
     </div>
